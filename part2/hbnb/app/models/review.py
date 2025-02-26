5fc24cff87fb9c6739f76847d9c54e30a1ea37f1
Nigel Feng <10083@holbertonstudents.com>
2025-02-26 15:12:29 +1100

    task 0: review

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -7,3 +7,34 @@ class Review(BaseModel):
         self.rating = rating
         self.place = place
         self.user = user
+
+    @property
+    def text(self):
+    
+    @text.setter
+    def text(self, value):
+
+
+    @property
+    def rating(self):
+        pass
+
+    @rating.setter
+    def rating(self, value):
+        pass
+
+    @property
+    def place(self):
+        pass
+
+    @place.setter
+    def place(self, value):
+        pass
+
+    @property
+    def user(self):
+        pass
+
+    @user.setter
+    def user(self, value):
+        pass
