71a6cb0b7d5aeb1acc5cfb2e8920b60d2328315b
Nigel Feng <10083@holbertonstudents.com>
2025-03-21 18:15:38 +1100

    task 10 done

--- part3/hbnb/app/models/place.py
+++ part3/hbnb/app/models/place.py
@@ -15,7 +15,7 @@ class Place(BaseModel):
     __tablename__ = 'places'
 
     _title = db.Column("title", db.String(50), nullable=False)
-    _description = db.Column("description", db.String(150), nullable=True)
+    _description = db.Column("description", db.String(150), nullable=False)
     _price = db.Column(db.Float(120), nullable=False)
     _latitude = db.Column("latitude", db.Float, nullable=False, unique=True)
     _longitude = db.Column("longitude", db.Float, nullable=False, unique=True)
