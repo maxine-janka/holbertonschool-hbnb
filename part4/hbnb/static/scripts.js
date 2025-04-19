cda5fe177f8ada54456a7fcfa702d7fe0ec53b15
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-04-19 18:00:41 +1000

    Filter price working

--- part4/hbnb/static/scripts.js
+++ part4/hbnb/static/scripts.js
@@ -1,5 +1,5 @@
+let places;
 async function fetchPlaces() {
-    let places;
     try {
         const response = await axios.get('http://127.0.0.1:5000/api/v1/places/', {
             headers: {
@@ -7,17 +7,19 @@ async function fetchPlaces() {
             }
         });
         places = response.data;
-        console.log(places);
+        //console.log(places);
+        
 
     } catch (error) {
         console.error('Error fetching places:', error);
         return
     }
     displayPlaces(places);
+   
 }
 
 function displayPlaces(places) {
-    console.log(places);
+    //console.log(places);
     const propertyGrid = document.getElementById('hbnb-property-grid');
     propertyGrid.innerHTML = '';
     places.forEach(place => {
@@ -25,15 +27,41 @@ function displayPlaces(places) {
         placeElement.className = 'property-grid-item';
         placeElement.innerHTML = ` <div class="property-grid-item-image property-image-1"></div>
             <ul class="property-grid-item-details">
-                <li class="property-grid-item-location">${place.title} (${place.latitude}, ${place.longitude})</li>
-                <li class="property-grid-item-description">${place.description || 'No description'}</li>
+                <li class="property-grid-item-location">${place.title}</li>
+                <li class="property-grid-item-description">${place.description}</li>
                 <li class="property-grid-item-price">$${place.price} per night</li>
             </ul>`;
         propertyGrid.appendChild(placeElement);
     });
 }
 
+// Filter place by price
+const priceFilter = document.getElementById('price-filter');
+    priceFilter.addEventListener('change', (event) => {
+        filterPlaceByPrice(event.target.value);
+    });
+
+function filterPlaceByPrice(selectedPrice) {
+    if (selectedPrice === 'all') {
+        displayPlaces(places);
+        return;
+    }
+
+    const price = parseInt(selectedPrice);
+    //console.log(price);
+
+    const filteredPlaces = places.filter(place => {
+        let Listedprice = place.price;
+        //console.log(Listedprice);
+        //console.log(places)
+        //Only return filtered place objects from places array
+        return Listedprice <= price;
+    });
+    //console.log(filteredPlaces)
+    displayPlaces(filteredPlaces);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Page loaded, calling fetchPlaces...');
+    //console.log('Page loaded, call fetchPlaces');
     fetchPlaces();
 });
