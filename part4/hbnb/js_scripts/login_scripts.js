5c2ebe49d9578d95e5972e8ecab16b51d185b51c
Nigel Feng <10083@holbertonstudents.com>
2025-04-20 01:12:48 +1000

    update

--- part4/hbnb/js_scripts/login_scripts.js
+++ part4/hbnb/js_scripts/login_scripts.js
@@ -5,35 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
         loginForm.addEventListener('submit', async (event) => {
             event.preventDefault();
             // Your code to handle form submission
-            let username = document.getElementById('username');
+            let email = document.getElementById('email');
             let password = document.getElementById('password');
-  
-            if (username.value == '' || password.value == '') {
-                alert('username or password empty');
-            } else {
-                alert('This form has been successfully submitted!');
-                console.log(`This form has a username of ${username.value} and password of ${password.value}`
-            );
-            }
+
+            alert('This form has been successfully submitted!');
+            console.log(`This form has a username of ${email.value} and password of ${password.value}`);
         });
     }
   });
-
-  async function loginUser(email, password) {
-    const response = await fetch('http://127.0.0.1:5000/api/v1/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-    });
-    // Handle the response
-    
-    if (response.ok) {
-        const data = await response.json();
-        document.cookie = `token=${data.access_token}; path=/`;
-        window.location.href = 'index.html';
-    } else {
-        alert('Login failed: ' + response.statusText);
-    }
-}
